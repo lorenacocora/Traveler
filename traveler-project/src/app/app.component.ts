d8134455c8cf3986db6ec430236cb855ed2669ab
Lorena <lorenacocora00@gmail.com>
2022-12-17 19:58:57 +0200

    Added login component

--- traveler-project/src/app/app.component.ts
+++ traveler-project/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import { Loader } from '@googlemaps/js-api-loader';
 
 
 @Component({
@@ -8,27 +7,8 @@ import { Loader } from '@googlemaps/js-api-loader';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent{
   
   title = 'traveler-project';
-
-  ngOnInit(): void {
-    //load the map in the browseer
-
-    let loader = new Loader({
-      apiKey:'AIzaSyBcisNaYJY8S3l2GF1l3q92aUzUlxRFHc0'
-    });
-
-    loader.load().then(()=>{
-      console.log("Loaded map!!!!");
-
-      new google.maps.Map(document.getElementById("map") as HTMLElement, {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 6,
-        streetViewControl: false,
-      });
-    })
-  }
-
 
 }
--- traveler-project/src/app/app.module.ts
+++ traveler-project/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule} from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
